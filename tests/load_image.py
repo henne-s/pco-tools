25f601fb046e161fbcea42bb780f219ddfb9f9d9
Hendrik <henne-s@gmx.de>
2017-02-05 15:39:09 +0100

    * Add doc strings * Cleanup

--- tests/load_image.py
+++ tests/load_image.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python
+"""Test pco_reader.load():
+Load a PCO image
+"""
 
-import pco_tools as pco
+from pco_tools import pco_reader as pco
 import matplotlib.pyplot as plt
 
+
 def main():
+    """Main function"""
+
     filename = 'images/dotmatrix.b16'
     image = pco.load(filename)
-    plt.imshow(image, cmap=plt.cm.jet)
+    plt.imshow(image,
+               cmap=plt.cm.jet,
+               interpolation='none',
+              )
     plt.show()
+
 
 if __name__ == '__main__':
     main()
