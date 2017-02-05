7666e8ab9fd10e7f6e5aad291e4d0a8f646b7d6c
Hendrik <henne-s@gmx.de>
2017-02-05 16:48:26 +0100

    * Add doc strings * Fix import statement

--- tests/show_info.py
+++ tests/show_info.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python
+"""Test pco_reader.info():
+Show information about a PCO image
+"""
 
-import pco_tools as pco
+from pco_tools import pco_reader as pco
+
 
 def main():
+    """Main function"""
+
     filename = 'images/dotmatrix.b16'
     pco.info(filename)
+
 
 if __name__ == '__main__':
     main()
